610a54ec8b7ccf0301bf22b34657458332ce5a1d
Sebastian Keller <sebkelle@phys.ethz.ch>
2017-03-24 18:06:53 -0700

    kicked out obsolete one_matrix

--- dmrg/framework/dmrg/block_matrix/detail/alps.hpp
+++ dmrg/framework/dmrg/block_matrix/detail/alps.hpp
@@ -24,14 +24,13 @@
  *
  *****************************************************************************/
 
-#ifndef MAQUIS_BLOCK_MATRIX_DEATAIL_ALPS_HPP
-#define MAQUIS_BLOCK_MATRIX_DEATAIL_ALPS_HPP
+#ifndef MAQUIS_BLOCK_MATRIX_DETAIL_ALPS_HPP
+#define MAQUIS_BLOCK_MATRIX_DETAIL_ALPS_HPP
 
 #include <alps/numeric/matrix.hpp>
 #include <alps/numeric/matrix/algorithms.hpp>
 #include <alps/numeric/diagonal_matrix.hpp>
 #include "dmrg/block_matrix/detail/alps_detail.hpp"
-#include "dmrg/block_matrix/detail/one_matrix.hpp"
 #include "utils/traits.hpp"
 
 namespace maquis { namespace traits {
@@ -46,18 +45,10 @@ namespace maquis { namespace traits {
 
 namespace alps { namespace numeric {
 
-    template<class Matrix> struct associated_one_matrix { };
     template<class Matrix> struct associated_dense_matrix { };
-
-    template<typename T, typename MemoryBlock>
-    struct associated_one_matrix<alps::numeric::matrix<T, MemoryBlock> > { typedef maquis::dmrg::one_matrix<T> type; };
 
     template<typename T, class MemoryBlock>
     struct associated_dense_matrix<alps::numeric::matrix<T, MemoryBlock> > { typedef alps::numeric::matrix<T, MemoryBlock> type; };
-
-    template<typename T>
-    struct associated_dense_matrix<maquis::dmrg::one_matrix<T> > { typedef alps::numeric::matrix<T> type; };
-
 } }
 
 #endif
