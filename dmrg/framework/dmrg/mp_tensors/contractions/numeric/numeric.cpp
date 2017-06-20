55417de16882f807b8edd54bdf29b6e06d82f5c2
Sebastian Keller <sebkelle@phys.ethz.ch>
2017-06-20 13:31:12 -0700

    no fma instructions in the release version

--- dmrg/framework/dmrg/mp_tensors/contractions/numeric/numeric.cpp
+++ dmrg/framework/dmrg/mp_tensors/contractions/numeric/numeric.cpp
@@ -66,9 +66,7 @@ inline void mydaxpy(std::size_t n, double a, const double* x, double* y)
   // broadcast the scale factor into a register
   __m256d x0 = _mm256_broadcast_sd(&a);
 
-  // align
-  //std::size_t xv = *reinterpret_cast<std::size_t*>(&x);
-  //std::size_t yv = *reinterpret_cast<std::size_t*>(&y);
+  // alignnment
   assert((uintptr_t)(x) % 32 == 0);
   assert((uintptr_t)(y) % 32 == 0);
 
@@ -77,9 +75,9 @@ inline void mydaxpy(std::size_t n, double a, const double* x, double* y)
   for (std::size_t i=0; i<ndiv4; ++i) {
     __m256d x1 = _mm256_load_pd(x+4*i);
     __m256d x2 = _mm256_load_pd(y+4*i);
-    //__m256d x3 = _mm256_mul_pd(x0, x1);
-    //__m256d x4 = _mm256_add_pd(x2, x3);
-    __m256d x4 = _mm256_fmadd_pd(x0, x1, x2);
+    __m256d x3 = _mm256_mul_pd(x0, x1);
+    __m256d x4 = _mm256_add_pd(x2, x3);
+    //__m256d x4 = _mm256_fmadd_pd(x0, x1, x2);
     _mm256_store_pd(y+4*i, x4);
   }
 
