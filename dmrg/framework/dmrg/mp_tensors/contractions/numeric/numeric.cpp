21fa075ffe1d2760ce12f625186ecd66e4254635
Sebastian Keller <sebkelle@phys.ethz.ch>
2017-10-03 14:19:37 -0700

    comment out experimental fma daxpy to avoid portability issues

--- dmrg/framework/dmrg/mp_tensors/contractions/numeric/numeric.cpp
+++ dmrg/framework/dmrg/mp_tensors/contractions/numeric/numeric.cpp
@@ -57,29 +57,29 @@ inline T round_up(T x)
     return (x+(A-1)) & (~(A-1));
 }
 
-inline void mydaxpy(std::size_t n, double a, const double* x, double* y)
-{
-  // broadcast the scale factor into a register
-  __m256d x0 = _mm256_broadcast_sd(&a);
-
-  // alignnment
-  assert((uintptr_t)(x) % ALIGNMENT == 0);
-  assert((uintptr_t)(y) % ALIGNMENT == 0);
-
-  std::size_t ndiv4 = n/4;
-
-  for (std::size_t i=0; i<ndiv4; ++i) {
-    __m256d x1 = _mm256_load_pd(x+4*i);
-    __m256d x2 = _mm256_load_pd(y+4*i);
-    //__m256d x3 = _mm256_mul_pd(x0, x1);
-    //__m256d x4 = _mm256_add_pd(x2, x3);
-    __m256d x4 = _mm256_fmadd_pd(x0, x1, x2);
-    _mm256_store_pd(y+4*i, x4);
-  }
-
-  for (std::size_t i=ndiv4*4; i < n ; ++i)
-    y[i] += a*x[i];
-}
+//inline void mydaxpy(std::size_t n, double a, const double* x, double* y)
+//{
+//  // broadcast the scale factor into a register
+//  __m256d x0 = _mm256_broadcast_sd(&a);
+//
+//  // alignnment
+//  assert((uintptr_t)(x) % ALIGNMENT == 0);
+//  assert((uintptr_t)(y) % ALIGNMENT == 0);
+//
+//  std::size_t ndiv4 = n/4;
+//
+//  for (std::size_t i=0; i<ndiv4; ++i) {
+//    __m256d x1 = _mm256_load_pd(x+4*i);
+//    __m256d x2 = _mm256_load_pd(y+4*i);
+//    //__m256d x3 = _mm256_mul_pd(x0, x1);
+//    //__m256d x4 = _mm256_add_pd(x2, x3);
+//    __m256d x4 = _mm256_fmadd_pd(x0, x1, x2);
+//    _mm256_store_pd(y+4*i, x4);
+//  }
+//
+//  for (std::size_t i=ndiv4*4; i < n ; ++i)
+//    y[i] += a*x[i];
+//}
 
 
 inline void blas_dgemm(const double* A, const double* B, double* C, int M, int K, int N, bool trA)
