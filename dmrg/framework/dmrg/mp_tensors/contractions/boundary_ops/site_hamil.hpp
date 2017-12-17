feb7fe5cd8fd75e0d415c4f7aa395f3519ddc193
Sebastian Keller <sebkelle@phys.ethz.ch>
2017-12-16 19:31:19 -0800

    work gpu tasks in separate thread for concurrency between gpu and cpu

--- dmrg/framework/dmrg/mp_tensors/contractions/boundary_ops/site_hamil.hpp
+++ dmrg/framework/dmrg/mp_tensors/contractions/boundary_ops/site_hamil.hpp
@@ -31,6 +31,61 @@
 namespace contraction {
 namespace common {
 
+    template <class Matrix, class OtherMatrix, class SymmGroup>
+    class gpu_work
+    {
+        typedef typename Matrix::value_type value_type;
+
+    public:
+        gpu_work(typename common::Schedule<Matrix, SymmGroup>::schedule_t const & tasks_
+                 , Boundary<OtherMatrix, SymmGroup> const & left_
+                 , Boundary<OtherMatrix, SymmGroup> const & right_
+                 , MPSTensor<Matrix, SymmGroup> & ket_tensor_
+                 , MPSTensor<Matrix, SymmGroup> & ret_gpu_
+                 )
+                 :  tasks(tasks_), left(left_), right(right_), ket_tensor(ket_tensor_), ret_gpu(ret_gpu_)
+                 {} 
+
+        void operator()()
+        {
+            storage::gpu::prefetch(ket_tensor);
+            storage::gpu::zero(ret_gpu);
+            storage::gpu::fetch(ket_tensor);
+
+            cudaEvent_t start, stop;
+            HANDLE_ERROR( cudaEventCreate(&start) );
+            HANDLE_ERROR( cudaEventCreate(&stop) );
+            HANDLE_ERROR( cudaEventRecord(start,0) );
+
+            for (unsigned tn = 0; tn < tasks.enumeration_gpu.size(); ++tn)
+            {
+                tasks[ boost::get<0>(tasks.enumeration_gpu[tn]) ]
+                     [ boost::get<1>(tasks.enumeration_gpu[tn]) ]
+                     [ boost::get<2>(tasks.enumeration_gpu[tn]) ]
+                    .contract_gpu(ket_tensor, left, right, (value_type*)ret_gpu.device_ptr[boost::get<0>(tasks.enumeration_gpu[tn])]);
+            }
+
+            HANDLE_ERROR( cudaEventRecord(stop,0) );
+
+            storage::gpu::evict(ret_gpu);
+
+            HANDLE_ERROR( cudaEventSynchronize(stop) );
+            float gpu_time;
+            HANDLE_ERROR( cudaEventElapsedTime( &gpu_time, start, stop ) );
+            tasks.gpu_time += gpu_time/1000;
+
+            storage::gpu::drop(ket_tensor);
+            storage::gpu::pin(ret_gpu);
+        }
+
+    private:
+        typename common::Schedule<Matrix, SymmGroup>::schedule_t const & tasks;
+        Boundary<OtherMatrix, SymmGroup> const & left;
+        Boundary<OtherMatrix, SymmGroup> const & right;
+        MPSTensor<Matrix, SymmGroup> & ket_tensor;
+        MPSTensor<Matrix, SymmGroup> & ret_gpu;
+    };
+
     template<class Matrix, class OtherMatrix, class SymmGroup>
     MPSTensor<Matrix, SymmGroup>
     site_hamil2(MPSTensor<Matrix, SymmGroup> & ket_tensor,
@@ -46,34 +101,13 @@ namespace common {
         typedef typename common::Schedule<Matrix, SymmGroup>::block_type::const_iterator const_iterator;
 
         ket_tensor.make_right_paired();
-        storage::gpu::prefetch(ket_tensor);
-
         MPSTensor<Matrix, SymmGroup> ret(ket_tensor.site_dim(), ket_tensor.row_dim(), ket_tensor.col_dim(),
                                          ket_tensor.data().basis(), RightPaired);
         MPSTensor<Matrix, SymmGroup> ret_gpu = ret;
 
-        storage::gpu::zero(ret_gpu);
-        storage::gpu::fetch(ket_tensor);
-
-        cudaEvent_t start, stop;
-        if (tasks.enumeration_gpu.size() && ket_tensor.device_ptr.size())
-        {
-            HANDLE_ERROR( cudaEventCreate(&start) );
-            HANDLE_ERROR( cudaEventCreate(&stop) );
-            HANDLE_ERROR( cudaEventRecord(start,0) );
-
-            for (index_type tn = 0; tn < tasks.enumeration_gpu.size(); ++tn)
-            {
-                tasks[ boost::get<0>(tasks.enumeration_gpu[tn]) ]
-                     [ boost::get<1>(tasks.enumeration_gpu[tn]) ]
-                     [ boost::get<2>(tasks.enumeration_gpu[tn]) ]
-                    .contract_gpu(ket_tensor, left, right, (value_type*)ret_gpu.device_ptr[boost::get<0>(tasks.enumeration_gpu[tn])]);
-            }
-
-            HANDLE_ERROR( cudaEventRecord(stop,0) );
-        }
-
-        storage::gpu::evict(ret_gpu);
+        boost::thread* gpu_worker;
+        if (tasks.enumeration_gpu.size())
+            gpu_worker = new boost::thread(gpu_work<Matrix, OtherMatrix, SymmGroup>(tasks, left, right, ket_tensor, ret_gpu));
 
         boost::chrono::high_resolution_clock::time_point now = boost::chrono::high_resolution_clock::now();
         #ifdef MAQUIS_OPENMP
@@ -88,21 +122,13 @@ namespace common {
         boost::chrono::high_resolution_clock::time_point then = boost::chrono::high_resolution_clock::now();
         tasks.cpu_time += boost::chrono::duration<double>(then - now).count();
 
-        if (tasks.enumeration_gpu.size() && ket_tensor.device_ptr.size())
+        if (tasks.enumeration_gpu.size())
         {
-            HANDLE_ERROR( cudaEventSynchronize(stop) );
-            float gpu_time;
-            HANDLE_ERROR( cudaEventElapsedTime( &gpu_time, start, stop ) );
-            tasks.gpu_time += gpu_time/1000;
-        }
-
-        storage::gpu::pin(ret_gpu);
-
-        if (tasks.enumeration_gpu.size() && ket_tensor.device_ptr.size())
+            gpu_worker->join();
             ret.data() += ret_gpu.data();
 
-        storage::gpu::drop(ket_tensor);
-        storage::gpu::drop(ret_gpu);
+            storage::gpu::drop(ret_gpu);
+        }
 
         ret.make_left_paired();
         return ret;
