265ed4ed5e03048dcafed265d75603b8d849a0f4
Sebastian Keller <sebkelle@phys.ethz.ch>
2017-12-19 11:51:52 -0800

    unified wrapper class for c++ dmrg sim instance

--- dmrg/lib/python/maquis/maquis.py
+++ dmrg/lib/python/maquis/maquis.py
@@ -3,122 +3,11 @@
 import libmaquis
 import numpy as np
 
-class dmrg:
-
-    def __init__(self, options, Hnp = None, Inp = None):
-
-        #self.options = { 
-        #                "nsweeps" : 8
-        #                , "max_bond_dimension" : 400
-        #                , "init_bond_dimension" : 400
-        #                  #
-        #                , "init_state" : 'default'
-        #                , "donotsave" : 1
-        #                , "chkp_each" : 100
-        #                , "measure_each" : 1
-        #                , "chkpfile" : 'chkp_dummy.h5'
-        #                , "resultfile" : 'result_dummy.h5'
-        #                  #"storagedir" : '/scratch/sebkelle/boundaries'
-        #                  #
-        #                , "ietl_jcd_maxiter" : 9
-        #                , "ietl_jcd_tol" : 1e-7
-        #                , "truncation_initial" : 1e-7
-        #                , "truncation_final" : 1e-7
-        #                , "symmetry" : 'su2u1'
-        #                  #
-        #                , "integral_cutoff" : 1e-20
-        #                , "conv_thresh" : 1e-6
-        #                  #
-        #                , "MEASURE[ChemEntropy]" : 1
-        #                , "MEASURE[1rdm]" : 1
-        #                , "MEASURE[2rdm]" : 1
-        #                , "MEASURE[Energy]" : 1
-        #               }
-
-        #if Hnp is not None and Inp is not None:
-        #    Ls = Inp.shape
-        #    assert(min(Ls) == max(Ls))
-        #    self.options["L"] = Ls[0]
-
-        #    Ecore = 0.0;
-        #    if options.has_key("Ecore"):
-        #        Ecore = options["Ecore"]
-
-        #    self.options["integrals"] = dmrg.pack_integrals(Hnp, Inp, self.options["L"], Ecore)
-
-        self.options.update(options)
-
-        # the C++ DMRG instance holding the wavefunction
-        self.solver = libmaquis.interface(self.options)
-
-    def optimize(self):
-        """calculate ground state wavefunction"""
-        self.solver.optimize()
-
-    def energy(self):
-        self.solver.measure("Energy")
-        return self.solver.getObservable("Energy")
-
-    def opdm(self):
-        """calculate the 1-body reduced density matrix"""
-
-        self.solver.measure("oneptdm")
-        r1 = self.solver.getObservable("oneptdm")
-        r1l = self.solver.getLabels("oneptdm")
-        rdm1 = dmrg.expand_1rdm(r1, r1l, self.options["L"])
-        return rdm1
-
-    def tpdm(self):
-        """calculate the 2-body reduced density matrix"""
-
-        self.solver.measure("twoptdm")
-        r2 = self.solver.getObservable("twoptdm")
-        r2l = self.solver.getLabels("twoptdm")
-        rdm2 = dmrg.expand_2rdm(r2, r2l, self.options["L"])
-        return rdm2
-
-    @staticmethod
-    def expand_1rdm(rdm, labels, L):
-        odm = np.zeros([L,L])
-
-        for lab, val in zip(labels, rdm):
-            i = lab[0]
-            j = lab[1]
-
-            odm[i,j] = val
-            odm[j,i] = val
-
-        return odm
-
-    @staticmethod
-    def expand_2rdm(rdm, labels, L):
-        odm = np.zeros([L,L,L,L])
-
-        for lab, val_raw in zip(labels, rdm):
-            i = lab[0]
-            j = lab[1]
-            k = lab[2]
-            l = lab[3]
-
-            # lightspeed doesnt use canonical ordering
-            j,l = l,j
-
-            val = 0.5 * val_raw
-            odm[i,j,k,l] = val
-
-            if l != k or i != j:
-                odm[j,i,l,k] = val
-
-            if min(i,j) != min(l,k) or max(i,j) != max(l,k):
-                odm[k,l,i,j] = val
-                if l != k or i != j:
-                    odm[l,k,j,i] = val
-
-        return odm
-
 class DummyCSF:
-    self.total_ndet = 0
-    self.total_nCSF = 0
+
+    def __init__(self):
+        self.total_ndet = 0
+        self.total_nCSF = 0
 
 
 class DMRGBox:
@@ -171,17 +60,30 @@ class DMRGBox:
     def compute_states(self, S_ind, S_nstate):
         for S in S_ind:
             self.options['spin'] = S
-            self.solvers[S] = dmrg(self.options)
+            self.solvers[S] = libmaquis.interface(self.options)
             self.solvers[S].optimize()
 
     def energy(self, S, state):
-        return self.solvers[S].energy()
+        self.solvers[S].measure("Energy")
+        return self.solvers[S].getObservable("Energy")
 
     def opdm(self, S, evecs1, evecs2, total):
-        return self.solvers[S].opdm()
+        """calculate the 1-body reduced density matrix"""
+
+        self.solvers[S].measure("oneptdm")
+        r1 = self.solvers[S].getObservable("oneptdm")
+        r1l = self.solvers[S].getLabels("oneptdm")
+        rdm1 = DMRGBox.expand_1rdm(r1, r1l, self.options["L"])
+        return rdm1
 
     def tpdm(self, S, evecs1, evecs2, symmetrize):
-        return self.solvers[S].tpdm()
+        """calculate the 2-body reduced density matrix"""
+
+        self.solvers[S].measure("twoptdm")
+        r2 = self.solvers[S].getObservable("twoptdm")
+        r2l = self.solvers[S].getLabels("twoptdm")
+        rdm2 = DMRGBox.expand_2rdm(r2, r2l, self.options["L"])
+        return rdm2
 
 
     # unimplemented CASBox functionality
@@ -211,6 +113,45 @@ class DMRGBox:
             if k != "integrals":
                 ifile.write(str(k) + ' = ' + str(v) + '\n')
         ifile.close()
+
+    @staticmethod
+    def expand_1rdm(rdm, labels, L):
+        odm = np.zeros([L,L])
+
+        for lab, val in zip(labels, rdm):
+            i = lab[0]
+            j = lab[1]
+
+            odm[i,j] = val
+            odm[j,i] = val
+
+        return odm
+
+    @staticmethod
+    def expand_2rdm(rdm, labels, L):
+        odm = np.zeros([L,L,L,L])
+
+        for lab, val_raw in zip(labels, rdm):
+            i = lab[0]
+            j = lab[1]
+            k = lab[2]
+            l = lab[3]
+
+            # lightspeed doesnt use canonical ordering
+            j,l = l,j
+
+            val = 0.5 * val_raw
+            odm[i,j,k,l] = val
+
+            if l != k or i != j:
+                odm[j,i,l,k] = val
+
+            if min(i,j) != min(l,k) or max(i,j) != max(l,k):
+                odm[k,l,i,j] = val
+                if l != k or i != j:
+                    odm[l,k,j,i] = val
+
+        return odm
 
 
     @staticmethod
