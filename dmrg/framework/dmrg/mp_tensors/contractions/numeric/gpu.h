0935567784a80fd35649722a7361c80c5b04628a
Sebastian Keller <sebkelle@phys.ethz.ch>
2018-02-02 11:02:08 -0800

    perform only one dgemm at contraction group level for all matrix groups

--- dmrg/framework/dmrg/mp_tensors/contractions/numeric/gpu.h
+++ dmrg/framework/dmrg/mp_tensors/contractions/numeric/gpu.h
@@ -37,10 +37,16 @@
 void dcopytr_gpu(cublasHandle_t handle, cudaStream_t stream,
                  unsigned ls, unsigned ms, unsigned rs, double* l_buf, GemmDotData<double> & gdd);
 
-void dgemm_ddot_gpu(cublasHandle_t handle, cudaStream_t stream,
-                    unsigned ls, unsigned ms, unsigned rs,
-                    const unsigned* b2sz, const double* t, double* ls_buf, double* out,
-                    GemmDotData<double> & gdd);
+void dsacc_gpu(cublasHandle_t handle, cudaStream_t stream,
+               unsigned ls, unsigned ms, unsigned rs,
+               const unsigned* b2sz, const double* t, double* s_buf, double* out,
+               GemmDotData<double> & gdd);
+
+void dgemm_gpu(cublasHandle_t handle,
+               cudaStream_t stream,
+               unsigned ls, unsigned ms, unsigned rs,
+               double* s_buffer, double* dev_out, GemmDotData<double> & gdd, double* l_buffer);
+
 
 void vgemm(cublasHandle_t handle, cudaStream_t stream, BatchGemmData<double> & batch, int, int, size_t, double*, double*, double*);
 
