d204ab03fc3f0dc32d897ffaafb2b6b92d4c43ae
Sebastian Keller <sebkelle@phys.ethz.ch>
2018-08-01 10:42:32 -0700

    removed debug statements from ss_optimize.h

--- dmrg/framework/dmrg/optimize/ss_optimize.hpp
+++ dmrg/framework/dmrg/optimize/ss_optimize.hpp
@@ -95,16 +95,6 @@ public:
             if (lr == +1 && site+2 <= L) Storage::prefetch(right_[site+2]);
             if (lr == -1 && site > 0)    Storage::prefetch(left_[site-1]);
             
-            assert( left_[site].reasonable() );    // in case something went wrong
-            assert( right_[site+1].reasonable() ); // in case something went wrong
-            
-            
-//            maquis::cout << "My size: " << std::endl;
-//            maquis::cout << "  left_: " << utils::size_of(left_.begin(), left_.end())/1024.0/1024 << std::endl;
-//            maquis::cout << "  right_: " << utils::size_of(right_.begin(), right_.end())/1024.0/1024 << std::endl;
-//            maquis::cout << "  MPS: " << utils::size_of(mps.begin(), mps.end())/1024.0/1024 << std::endl;
-//            maquis::cout << "  MPS[i]: " << utils::size_of(mps[site])/1024.0/1024 << std::endl;
-            
             boost::chrono::high_resolution_clock::time_point now, then;
 
             std::pair<double, MPSTensor<Matrix, SymmGroup> > res;
