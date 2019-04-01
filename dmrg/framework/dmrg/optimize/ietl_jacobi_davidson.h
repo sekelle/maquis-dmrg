233f8e5b65df0e05d7f62530b6ab664f79b9a76a
Sebastian Keller <keller@cscs.ch>
2019-04-01 11:25:17 +0200

    updated alternative jd solver to current interface

--- dmrg/framework/dmrg/optimize/ietl_jacobi_davidson.h
+++ dmrg/framework/dmrg/optimize/ietl_jacobi_davidson.h
@@ -31,7 +31,6 @@
 #include "ietl_interface.h"
 
 #include <ietl/jacobi.h>
-#include <ietl/jd.h>
 
 template<class Matrix, class OtherMatrix, class SymmGroup>
 std::pair<double, MPSTensor<Matrix, SymmGroup> >
@@ -74,41 +73,44 @@ solve_ietl_jcd(SiteProblem<Matrix, OtherMatrix, SymmGroup> & sp,
     return r0;
 }
 
-/*template<class Matrix, class OtherMatrix, class SymmGroup>
-std::pair<double, MPSTensor<Matrix, SymmGroup> >
-solve_ietl_new_jd(SiteProblem<Matrix, OtherMatrix, SymmGroup> & sp,
-                  MPSTensor<Matrix, SymmGroup> const & initial,
-                  BaseParameters & params)
-{
-    typedef MPSTensor<Matrix, SymmGroup> Vector;
-    typedef SiteProblem<Matrix, OtherMatrix, SymmGroup> Operator;
-    typedef SingleSiteVS<Matrix, SymmGroup> Vecspace;
-    Vecspace vs(initial);
-
-    int n_evals = 1;   // number of eigenpairs to be calculated
-    int max_iter = params["ietl_jcd_maxiter"]; // maximal number of iterations
-
-    int m_max = 40;
-    int m_min = 20;
-
-    // tolerance
-    double rel_tol = params["ietl_jcd_tol"];
-    double abs_tol = rel_tol;
-
-    // maximal iterations for the correction equation solver
-    unsigned max_cor_iter = params["ietl_jcd_gmres"];
-
-    ietl::jd_iteration<double> iter(max_iter, m_min, m_max, rel_tol, abs_tol);
-    ietl::jd<Operator, Vecspace> jd(sp, vs);
-
-    // the correction equation solver must be an function object
-    ietl::ietl_gmres gmres(max_cor_iter);
-
-    jd.eigensystem(iter, initial, n_evals, gmres);
-    
-    std::vector<double> evals = jd.eigenvalues();
-    
-    return std::make_pair(evals[0], jd.eigenvector(0));
-}*/
+//#include <ietl/jd.h>
+//
+//template<class Matrix, class OtherMatrix, class SymmGroup>
+//std::pair<double, MPSTensor<Matrix, SymmGroup> >
+//solve_ietl_jcd(SiteProblem<Matrix, OtherMatrix, SymmGroup> & sp,
+//               MPSTensor<Matrix, SymmGroup> const & initial,
+//               BaseParameters & params,
+//               std::vector<MPSTensor<Matrix, SymmGroup> > ortho_vecs = std::vector<MPSTensor<Matrix, SymmGroup> >())
+//{
+//    typedef MPSTensor<Matrix, SymmGroup> Vector;
+//    typedef SiteProblem<Matrix, OtherMatrix, SymmGroup> Operator;
+//    typedef SingleSiteVS<Matrix, SymmGroup> Vecspace;
+//    Vecspace vs(initial, ortho_vecs);
+//
+//    int n_evals = 1;   // number of eigenpairs to be calculated
+//    int max_iter = params["ietl_jcd_maxiter"]; // maximal number of iterations
+//
+//    int m_max = 40;
+//    int m_min = 20;
+//
+//    // tolerance
+//    double rel_tol = params["ietl_jcd_tol"];
+//    double abs_tol = rel_tol;
+//
+//    // maximal iterations for the correction equation solver
+//    unsigned max_cor_iter = params["ietl_jcd_gmres"];
+//
+//    ietl::jd_iteration<double> iter(max_iter, m_min, m_max, rel_tol, abs_tol);
+//    ietl::jd<Operator, Vecspace> jd(sp, vs);
+//
+//    // the correction equation solver must be an function object
+//    ietl::ietl_gmres gmres(max_cor_iter);
+//
+//    jd.eigensystem(iter, initial, n_evals, gmres);
+//    
+//    std::vector<double> evals = jd.eigenvalues();
+//    
+//    return std::make_pair(evals[0], jd.eigenvector(0));
+//}
 
 #endif
